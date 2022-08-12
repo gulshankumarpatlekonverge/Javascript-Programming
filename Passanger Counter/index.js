a3eb5514e776399b24344521a7a67d00ad2d485b
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-08-12 13:02:31 +0530

    Function that logs sum

--- Passanger Counter/index.js
+++ Passanger Counter/index.js
@@ -20,6 +20,20 @@ function helloWorld(){
 
 helloWorld();
 
+//Function that logs sum
+
+let lap1 = 34;
+let lap2 = 33;
+let lap3 = 36;
+
+function logSum(){
+    let logSum = lap1 + lap2 + lap3;
+    console.log(logSum);
+//    console.log(lap1 + lap2 + lap3);
+}
+
+logSum()
+
 
 /*
 Example during explanantion
