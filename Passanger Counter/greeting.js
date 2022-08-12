4d4ae131b3a9a0ab40de5e2f90b70f7a2eaed666
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-08-12 19:57:19 +0530

    Render welcome message

---
+++ Passanger Counter/greeting.js
@@ -0,0 +1,10 @@
+let welcomeEl = document.getElementById("welcome-element");
+
+let myName = "Gulshankumar Patle";
+let greeting = "Good Morning, Welcome to Greeting DashBoard.";
+
+welcomeEl.innerText = myName + ", " + greeting;
+
+welcomeEl.innerText = welcomeEl.innerText + "😀";
+
+//welcomeEl.innerText += "😀";
