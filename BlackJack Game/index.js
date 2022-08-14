b5beba0507e084122d6c2c54818e31e43e224889
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-08-14 12:24:50 +0530

    BlackJack Game

---
+++ BlackJack Game/index.js
@@ -0,0 +1,41 @@
+var firstCard = 10;
+var secondCard = 5;
+
+var hasBlackJack = false;
+var isAlive = true;
+
+var message = "";
+var sum = firstCard + secondCard;
+
+var messageEl = document.getElementById("message-el");
+var sumEl = document.getElementById("sum-el");
+var cardsEl = document.getElementById("card-el");
+
+// DOM - querySelector("#...") using id attribute in html
+// var sumEl = document.querySelector("#sum-el");
+
+// DOM - querySelector(".----") using class attribute in html
+// var sumEl = document.querySelector(".sum-el");
+
+function startGame(newCard){
+    let newCards = newCard;
+
+    if(sum <= 20){ 
+        message = "Do you want to draw new card?";
+    }else if(sum === 21){
+        message = "You have got BlackJack!";
+        hasBlackJack = true;
+    }else{
+        message = "You're out of the Game...!";
+        isAlive = false;
+    }
+    messageEl.textContent = message;
+    sumEl.textContent = "Sum : "+ sum;
+    cardsEl.textContent = "Cards : " + firstCard +" "+ secondCard + " " + newCards;    
+}
+
+function addNewCard(){
+    var newCard = 6;
+    sum += newCard;
+    startGame(newCard);
+}
